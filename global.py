079f3685ef7855980a71e108f68042375ac873ef
Carlos Zaldviar Batista <carlos.zaldivar.batista@gmail.com>
2018-01-21 00:25:47 +0100

    Add sklearn PCA.

--- global.py
+++ global.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import cv2
-import glob
+import glob2
 from itertools import chain
 import pickle
 import shutil
@@ -9,6 +9,7 @@ import numpy as np
 import pathlib
 
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 counter = 1
 def img_to_combined_histogram(img):
@@ -26,15 +27,12 @@ def img_to_combined_histogram(img):
     sat_hist, _ = np.histogram(sat, bins=256)
     val_hist, _ = np.histogram(val, bins=256)
 
-    return np.array(hue_hist + sat_hist + val_hist)
+    return np.append(np.append(hue_hist, sat_hist), val_hist)
 
 
 def get_image_paths(directory):
-    image_paths = []
-    for filename in glob.iglob(directory + '**/**', recursive=True):
-        if not os.path.isdir(filename):
-            image_paths.append(filename)
-    return image_paths
+    paths = glob2.glob(directory + '**')
+    return [path for path in paths if not os.path.isdir(path)]
 
 
 def read_images(image_paths):
@@ -45,33 +43,53 @@ def read_images(image_paths):
     return images
 
 
-def main(argv):
-    if len(argv) == 1:
-        print("Provide directory with images.")
-        return
+def get_params(argv):
+    directory = n_clusters = None
+    if len(argv) > 1:
+        directory = sys.argv[1]
+    if len(argv) > 2:
+        n_clusters = int(sys.argv[2])
+    return directory, n_clusters
 
-    directory = sys.argv[1]
-    image_paths = get_image_paths(directory)
-    images = read_images(image_paths)
 
+def get_histograms(images):
     # Read histograms from cache if it's available
-    if os.path.exists("cache"):
-        with open("cache", "rb") as fp:
+    if os.path.exists("cache_normal"):
+        with open("cache_normal", "rb") as fp:
             histograms = pickle.load(fp)
     else:
         histograms = [img_to_combined_histogram(img) for img in images]
         # Save histograms to not recalculate them next time
-        with open("cache", "wb") as fp:
+        with open("cache_normal", "wb") as fp:
             pickle.dump(histograms, fp)
+    return histograms
 
-    if len(argv) == 3:
-        n_clusters = int(sys.argv[2])
+
+def get_reduced_histograms(normal_histograms):
+    # Read reduced histograms from cache if it's available
+    if os.path.exists("cache_pca"):
+        with open("cache_pca", "rb") as fp:
+            reduced_histograms = pickle.load(fp)
     else:
-        n_clusters = 21
+        reduced_histograms = PCA(n_components='mle', svd_solver='full').fit_transform(normal_histograms)
+        # Save histograms to not recalculate them next time
+        with open("cache_pca", "wb") as fp:
+            pickle.dump(reduced_histograms, fp)
+    return reduced_histograms
 
+
+def basic_kmeans(histograms, image_paths, n_clusters):
     kmeans = KMeans(n_clusters=n_clusters).fit_predict(histograms)
+    write_results('results_global/basic/', kmeans, image_paths)
 
-    result_directory = "results_global/"
+
+def pca_kmeans(histograms, image_paths, n_clusters):
+    histograms_pca = get_reduced_histograms(histograms)
+    kmeans = KMeans(n_clusters=n_clusters).fit_predict(histograms_pca)
+    write_results('results_global/pca/', kmeans, image_paths)
+
+
+def write_results(result_directory, kmeans, image_paths):
     if os.path.exists(result_directory):
         shutil.rmtree(result_directory)
     for kmean, image_path in zip(kmeans, image_paths):
@@ -79,6 +97,23 @@ def main(argv):
         cluster_dir = result_directory + str(kmean)
         pathlib.Path(cluster_dir).mkdir(parents=True, exist_ok=True)
         shutil.copyfile(image_path, cluster_dir + '/' + os.path.basename(image_path))
+
+
+def main(argv):
+    directory, n_clusters = get_params(argv)
+    if directory is None:
+        print("Provide directory with images.")
+        return
+
+    image_paths = get_image_paths(directory)
+    images = read_images(image_paths)
+    histograms = get_histograms(images)
+
+    if n_clusters is None:
+        n_clusters = 21
+
+    # basic_kmeans(histograms, image_paths, n_clusters)
+    pca_kmeans(histograms, image_paths, n_clusters)
 
 
 if __name__ == "__main__":
